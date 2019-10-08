c26a7000fafd783f8d0bd10d5bc7dc71ce85d76b
Ponchimeow <trancetoradora@gmail.com>
2019-10-08 10:01:27 +0800

    use gulp responsive

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,57 @@ const gulp = require('gulp'),
   rename = require('gulp-rename'),
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify-es').default,
-  minHTML = require('gulp-htmlmin');
+  minHTML = require('gulp-htmlmin'),
+  responsive = require('gulp-responsive');
 
 gulp.task('image', (done) => {
   gulp.src('./image/**')
     .pipe(plumber())
+    .pipe(responsive({
+      'bg_header.png': {
+        width: '50%',
+        quality: 60
+      },
+      'bg_*.png': {
+        width: '50%',
+        quality: 60
+      },
+      'bg_*.jpg': {
+        width: '50%',
+        quality: 60
+      },
+      'icon-community.png': {
+        width: 80,
+        quality: 60
+      },
+      'icon-member.png': {
+        width: 300,
+        quality: 60
+      },
+      'judge_*.jpg': {
+        width: '60%',
+        quality: 60
+      },
+      'judge_*_l.jpg': {
+        width: '50%',
+        quality: 60
+      },
+      'team_*.jpg': {
+        width: 700,
+        quality: 60
+      },
+      'logo_nav.png': {
+        quality: 60
+      },
+      'title.png': {
+        width: 120,
+        quality: 60
+      },
+      'add-circular-outlined-button.png': {
+        width: 30,
+        quality: 60
+      }
+    }))
     .pipe(gulpImagemin())
     .pipe(webp())
     .pipe(gulp.dest('./build/image'));
